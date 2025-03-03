94791aa1e4349da51a29482fdc03166ecc0354ef
safouuwa <safou1406@hotmail.com>
2025-03-03 16:10:12 +0100

    Template Project

---
+++ client/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+using LibData;
+
+// SendTo();
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClientUDP.start();
+    }
+}
+
+public class Setting
+{
+    public int ServerPortNumber { get; set; }
+    public string? ServerIPAddress { get; set; }
+    public int ClientPortNumber { get; set; }
+    public string? ClientIPAddress { get; set; }
+}
+
+class ClientUDP
+{
+
+    //TODO: [Deserialize Setting.json]
+    static string configFile = @"../Setting.json";
+    static string configContent = File.ReadAllText(configFile);
+    static Setting? setting = JsonSerializer.Deserialize<Setting>(configContent);
+
+
+    public static void start()
+    {
+
+        //TODO: [Create endpoints and socket]
+
+
+        //TODO: [Create and send HELLO]
+
+        //TODO: [Receive and print Welcome from server]
+
+        // TODO: [Create and send DNSLookup Message]
+
+
+        //TODO: [Receive and print DNSLookupReply from server]
+
+
+        //TODO: [Send Acknowledgment to Server]
+
+        // TODO: [Send next DNSLookup to server]
+        // repeat the process until all DNSLoopkups (correct and incorrect onces) are sent to server and the replies with DNSLookupReply
+
+        //TODO: [Receive and print End from server]
+
+
+
+
+
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+using LibData;
+
+// ReceiveFrom();
+class Program
+{
+    static void Main(string[] args)
+    {
+        ServerUDP.start();
+    }
+}
+
+public class Setting
+{
+    public int ServerPortNumber { get; set; }
+    public string? ServerIPAddress { get; set; }
+    public int ClientPortNumber { get; set; }
+    public string? ClientIPAddress { get; set; }
+}
+
+
+class ServerUDP
+{
+    static string configFile = @"../Setting.json";
+    static string configContent = File.ReadAllText(configFile);
+    static Setting? setting = JsonSerializer.Deserialize<Setting>(configContent);
+
+    // TODO: [Read the JSON file and return the list of DNSRecords]
+
+
+
+
+    public static void start()
+    {
+
+
+        // TODO: [Create a socket and endpoints and bind it to the server IP address and port number]
+
+
+
+        // TODO:[Receive and print a received Message from the client]
+
+
+
+
+        // TODO:[Receive and print Hello]
+
+
+
+        // TODO:[Send Welcome to the client]
+
+
+        // TODO:[Receive and print DNSLookup]
+
+
+        // TODO:[Query the DNSRecord in Json file]
+
+        // TODO:[If found Send DNSLookupReply containing the DNSRecord]
+
+
+
+        // TODO:[If not found Send Error]
+
+
+        // TODO:[Receive Ack about correct DNSLookupReply from the client]
+
+
+        // TODO:[If no further requests receieved send End to the client]
+
+    }
+
+
+}
